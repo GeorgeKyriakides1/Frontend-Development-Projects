dec0f96272172599b33498e361996e419929c206
GeorgeKyriakides1 <88119054+GeorgeKyriakides1@users.noreply.github.com>
2023-04-21 22:38:44 +1000

    Add files via upload

---
+++ swapValues/swapValues.js
@@ -0,0 +1,11 @@
+/* Problem - Swap the following values, A = 10 and B = 20, without using a third variable. */
+
+function swapValues(A = 0, B = 0) {
+    A = A + B; // A = 30
+    B = A - B; // B = 10
+    A = A - B; // A = 20
+    return A + ", " + B; // 20, 10
+}
+
+// Output to console. 
+console.log(swapValues(10, 20));
